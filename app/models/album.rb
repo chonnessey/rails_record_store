716bab78e8faf7fb9a323681e5000584b7fe2184
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-04 09:01:50 -0700

    add migration for join table

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
+  has_and_belongs_to_many :artists
   validates :name, presence: true
   validates_length_of :name, maximum: 100
 
